b486c01847ebc41075d5e7aeb14575797118902b
hasan <mdhasanhk.98@gmail.com>
2022-10-13 02:31:52 +0600

    t-shirt

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import useTShirt from '../../hooks/usetshirt';
+import TShirt from '../TShirt/TShirt'
+import Cart from '../Cart/Cart'
+import './Home.css'
 
 const Home = () => {
+    const [tshirts, setTShirts] = useTShirt();
+
     return (
-        <div>
-            <h3>This is home</h3>
+        <div className='home-container'>
+            <div className="tshirt-container">
+                {
+                    tshirts.map(tshirt => <TShirt key={tshirt._id} tshirt={tshirt}></TShirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
